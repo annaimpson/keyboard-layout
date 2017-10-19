b1ac73ea3bc5de27e22b75bcee7b2c2855a08fa8
Anna Impson <annaimpson@gmail.com>
2017-10-19 11:24:16 -0400

    reworked the keyboard layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,34 +2,6 @@ function keyboardFunc(e){
   e.preventDefault();
   var x = e.keyCode;
   var textarea = document.getElementsByTagName("textarea")[0];
-  // var iso = {
-  //    81: 'q',
-  //    87: 'w',
-  //    69: 'e',
-  //    82: 'r',
-  //    84: 't',
-  //    89: 'y',
-  //    85: 'u',
-  //    73: 'i',
-  //    79: 'o',
-  //    80: 'p',
-  //    65: 'a',
-  //    83: 's',
-  //    68: 'd',
-  //    70: 'f',
-  //    71: 'g',
-  //    72: 'h',
-  //    74: 'j',
-  //    75: 'k',
-  //    76: 'l',
-  //    90: 'z',
-  //    88: 'x',
-  //    67: 'c',
-  //    86: 'v',
-  //    66: 'b',
-  //    78: 'n',
-  //    77: 'm'
-  // };
   var dvorak = {
     82: 'p',
     84: 'y',
@@ -58,21 +30,13 @@ function keyboardFunc(e){
     190: 'v',
     191: 'z',
   };
-  var map = new Map(Object.entries(dvorak));
-  var dvorakObject = Object.entries(dvorak);
-  // var isoObject = Object.entries(iso);
-  console.log(dvorakObject);
 
     function keyboardLayout() {
-      for (var i = 1; i < dvorakObject.length; i++) {
-        if( e.keyCode == dvorakObject[i][0]) {
-          console.log(dvorakObject[i][0]);
-          textarea.append(dvorakObject[i][1]);
-          console.log(dvorakObject[i][1]);
-        }
+      if (dvorak[x]) {
+        textarea.append(dvorak[x]);
       }
-  }
-  keyboardLayout();
+    }
+    keyboardLayout();
 }
 
 var myEl = document.getElementById('textarea');
